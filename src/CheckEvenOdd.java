b01da6cb38b4975a5e316c848c16c8c88d67f7e6
Wayne <jwlin2014@gmail.com>
2017-05-17 20:09:25 -0700

    Update CheckEvenOdd.java

--- src/CheckEvenOdd.java
+++ src/CheckEvenOdd.java
@@ -8,7 +8,6 @@ public class CheckEvenOdd {
 	public boolean isEven(int num) {
 		boolean result = num % 2 == 0 ? true : false;
 		return result;
-
 	}
 
 	//check if the number can be divided by three
@@ -21,13 +20,10 @@ public class CheckEvenOdd {
 	public void detectNumber(int[] a) {
 		int n = a.length;
 		for (int i = 0; i < n; i++) {
-
 			int num = a[i];
 			// case 1: num is even number. If num is even number and can be divided by three, print eventhree. Otherwise, print num.
 			if (isEven(num)) {
-
 				System.out.println(isDivisiblebyThree(num) == true ? eventhree : num);
-
 			// case 2: num is odd number. If num is odd number and can be divided by three, print odd. Otherwise, print num.
 			} else {
 				System.out.println(isDivisiblebyThree(num) == true ? three : odd);
