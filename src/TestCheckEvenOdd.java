3fe53f06eb85e53f7375ad2d53c483d4434c2118
Wayne <jwlin2014@gmail.com>
2017-05-17 20:08:12 -0700

    add more test cases

--- src/TestCheckEvenOdd.java
+++ src/TestCheckEvenOdd.java
@@ -10,37 +10,47 @@ import org.junit.Test;
 
 
 public class TestCheckEvenOdd {
-
+    
     CheckEvenOdd _checkEvenOdd = new CheckEvenOdd();
-
-	@Test
-	public void testEven() {
-		
-		boolean result1 = _checkEvenOdd.isEven(4);
-		boolean expected1 = true;
-		boolean result2 = _checkEvenOdd.isEven(843);
-	    boolean expected2 = false;
-	    assertEquals(expected1, result1);
-	    assertEquals(expected2,result2);
-		
-	}
-	@Test
-    public void testDivisibleThree(){
-    	
-    	boolean result1 = _checkEvenOdd.isDivisiblebyThree(3);
-    	boolean expected1 = true; 
-    	boolean result2 = _checkEvenOdd.isDivisiblebyThree(340231);
-    	boolean expected2 = false;
-    	assertEquals(expected1, result1);
-	    assertEquals(expected2,result2);
-    	
+    
+    @Test
+    public void testEven() {
+        
+        boolean result1 = _checkEvenOdd.isEven(4);
+        boolean expected1 = true;
+        boolean result2 = _checkEvenOdd.isEven(843);
+        boolean expected2 = false;
+        boolean result3 = _checkEvenOdd.isEven(-1);
+        boolean expected3 = false;
+        boolean result4 = _checkEvenOdd.isEven(0);
+        boolean expected4 = true;
+        
+        assertEquals(expected1, result1);
+        assertEquals(expected2,result2);
+        assertEquals(expected3,result3);
+        assertEquals(expected4,result4);
+        
     }
-	@Test
-	public void testArray(){
-		int[] a = {-3,2,5,1,3,0,1000};
-		_checkEvenOdd.detectNumber(a);
-	}
-	
-	
-
+    @Test
+    public void testDivisibleThree(){
+        
+        boolean result1 = _checkEvenOdd.isDivisiblebyThree(3);
+        boolean expected1 = true;
+        boolean result2 = _checkEvenOdd.isDivisiblebyThree(340231);
+        boolean expected2 = false;
+        boolean result3 = _checkEvenOdd.isDivisiblebyThree(6);
+        boolean expected3 = true;
+        assertEquals(expected1, result1);
+        assertEquals(expected2,result2);
+        assertEquals(expected3,result3);
+        
+    }
+    @Test
+    public void testArray(){
+        int[] a = {-3,2,5,1,3,0,1000};
+        _checkEvenOdd.detectNumber(a);
+    }
+    
+    
+    
 }
